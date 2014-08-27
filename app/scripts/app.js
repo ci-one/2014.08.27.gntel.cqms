1fa883d4642c2b0de668af4212696ce0181203f3
seongyu <suengyu@me.com>
2014-08-27 18:00:56 +0900

    datatables 사용을 위한 레포지터리 추가

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,11 +16,51 @@ angular.module('gntelCqmsApp', [
                 redirectTo: '/quality/weather_chart'
             })
             .when('/quality/weather_chart', {
-                templateUrl: '../views/contents/052_S_2100_quality.weather.chart.html'
+                templateUrl: 'views/contents/052_S_2100_quality.weather.chart.html'
             })
             .when('/probe/list', {
                 templateUrl: 'views/contents/052_S_2110_probe.list.html'
             })
+
+
+
+
+            //추가:김선규
+            .when('/priodic', {
+                templateUrl: 'views/contents/052_S_quality_check_priodic.html'
+            })
+            .when('/priodic/result', {
+                templateUrl: 'views/contents/052_S_quality_check_probe_plcy_info.html'
+            })
+            .when('/priodic/plcy', {
+                templateUrl: 'views/contents/052_S_quality_check_rule_plcy_list.html'
+            })
+            .when('/demand', {
+                templateUrl: 'views/contents/052_S_quality_check_demand.html'
+            })
+            .when('/demand/process', {
+                templateUrl: 'views/contents/052_S_quality_check_demand_process.html'
+            })
+            .when('/demand/result', {
+                templateUrl: 'views/contents/052_S_quality_check_demand_result.html'
+            })
+            .when('/current', {
+                templateUrl: 'views/contents/052_S_quality_check_current_list.html'
+            })
+            .when('/current/plcy', {
+                templateUrl: 'views/contents/052_S_quality_check_current_plcy_info.html'
+            })
+            .when('/priodicR', {
+                templateUrl: 'views/contents/052_S_quality_result_priodic.html'
+            })
+            .when('/priodicavg', {
+                templateUrl: 'views/contents/052_S_quality_result_priodic_avg_list.html'
+            })
+            //종료:김선규
+
+
+
+
             .when('/probe/plcy_list', {
                 templateUrl: 'views/contents/052_S_2210_probe.plcy.list.html'
             })
@@ -37,5 +77,5 @@ angular.module('gntelCqmsApp', [
                 redirectTo: '/'
             });
 
-        $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
     });
