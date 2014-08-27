dd2147f01b52f7da4ae0b7d67a892eaf186eba4a
juree <ree>
2014-08-27 15:15:45 +0900

    probe.list / probe.plcy.list 작업중 채영범

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
     },
     open: {
       server: {
-        url: 'http://localhost:<%= express.options.port %>'
+        url: 'http://192.168.1.13:<%= express.options.port %>'
       }
     },
     watch: {
@@ -162,7 +162,7 @@ module.exports = function (grunt) {
     'node-inspector': {
       custom: {
         options: {
-          'web-host': 'localhost'
+          'web-host': '192.168.1.13'
         }
       }
     },
@@ -184,7 +184,7 @@ module.exports = function (grunt) {
             // opens browser on initial server start
             nodemon.on('config:update', function () {
               setTimeout(function () {
-                require('open')('http://localhost:8080/debug?port=5858');
+                require('open')('http://192.168.1.13:8080/debug?port=5858');
               }, 500);
             });
           }
